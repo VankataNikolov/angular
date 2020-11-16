74be7c1e5d336e9d061447d311e83640a6d1a6e4
vankata83 <lolapaluza@mail.bg>
2020-11-16 21:49:31 +0200

    add get all themes functionality

--- my-first-app/src/app/main/main.component.ts
+++ my-first-app/src/app/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ITheme } from '../interfaces';
+import { ThemeService } from '../theme.service';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  themes: ITheme[];
+
+  constructor(private themeService: ThemeService) { }
 
   ngOnInit(): void {
+    this.themeService.getThemeList().subscribe(
+      themeList => this.themes = themeList
+    )
   }
 
 }
